5cfeb1ca8169ef7dcf358e5f305e2d90d5be1bc6
Merch-A <95261605+ElMercH1@users.noreply.github.com>
2025-07-17 15:11:33 -0500

    NEW - NAVBAR

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,49 +1,21 @@
-import { useState } from 'react'
 import './CSS/navbar.css'
 
-const menuItems = [
-  { id: 'home', label: 'Inicio'},
-  { id: 'about', label: 'Sobre mí'},
-  { id: 'abilities', label: 'Proyectos'},
-  { id: 'contact', label: 'Contacto'},
-]
 
 function App() {
-  const [open, setOpen] = useState(false)
-
-  const handleSelect = (id) => {
-    setOpen(false)
-    
-    // Scroll suave hacia la sección seleccionada
-    const element = document.getElementById(id)
-    if (element) {
-      element.scrollIntoView({ 
-        behavior: 'smooth',
-        block: 'start'
-      })
-    }
-  }
-
-
   return (
-    <div id='contenedor_main'>
-      {/* Botón de menú en la parte superior izquierda */}
-      <div id='boton_hamburguesa' onClick={() => setOpen(!open)} className={open ? 'active' : ''}>
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
 
-      {/* Menú desplegable */}
-      {open && (
-        <div id='contenedor_navbar'>
-          {menuItems.map(item => (
-            <div id='texto_navbar' key={item.id} onClick={() => handleSelect(item.id)}>
-              {item.label}
-            </div>
-          ))}
-        </div>
-      )}
+    <div id="contenedor_main">
+      
+      {/* Header */}
+      <header id="header">
+        <nav className="nav-links">
+          <a href="#home">Inicio</a>
+          <a href="#about">Sobre mí</a>
+          <a href="#abilities">Proyectos</a>
+          <a href="#contact">Contacto</a>
+        </nav>
+      </header>
+
 
       <section id='home' className='titulos_secciones'>
         Seccion Inicio
