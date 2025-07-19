998a2588004b54869fd4279065713993fc7fa441
Merch-A <95261605+ElMercH1@users.noreply.github.com>
2025-07-19 12:06:32 -0500

    SAVE - NEW NAVBAR

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,15 +7,79 @@ function App() {
     <div id="contenedor_main">
       
       {/* top_navbar */}
-      <top_navbar id="top_navbar">
-        <nav className="nav-links">
-          <a href="#home">Inicio</a>
-          <a href="#about">Sobre mí</a>
-          <a href="#abilities">Proyectos</a>
-          <a href="#contact">Contacto</a>
-        </nav>
-      </top_navbar>
+      <div class='wrapper'>
+        <div class='liquidglass-wrapper dock'>
 
+          <div class='liquidglass-effect'></div>
+          <div class='liquidglass-tint'></div>
+          <div class='liquidglass-shine'></div>
+          <div class='liquidglass-text'>
+            <div class='dock'>
+              <a href="#home">Inicio</a>
+              <a href="#about">Sobre mí</a>
+              <a href="#abilities">Proyectos</a>
+              <a href="#contact">Contacto</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* SVG Filter para efectos de vidrio */}
+      <svg style={{display: "none"}}>
+        <filter
+          id="glass-distortion"
+          x="0%"
+          y="0%"
+          width="100%"
+          height="100%"
+          filterUnits="objectBoundingBox"
+        >
+          <feTurbulence
+            type="fractalNoise"
+            baseFrequency="0.01 0.01"
+            numOctaves="1"
+            seed="5"
+            result="turbulence"
+          />
+
+          <feComponentTransfer in="turbulence" result="mapped">
+            <feFuncR type="gamma" amplitude="1" exponent="10" offset="0.5" />
+            <feFuncG type="gamma" amplitude="0" exponent="1" offset="0" />
+            <feFuncB type="gamma" amplitude="0" exponent="1" offset="0.5" />
+          </feComponentTransfer>
+
+          <feGaussianBlur in="turbulence" stdDeviation="3" result="softMap" />
+
+          <feSpecularLighting
+            in="softMap"
+            surfaceScale="5"
+            specularConstant="1"
+            specularExponent="100"
+            lightingColor="white"
+            result="specLight"
+          >
+            <fePointLight x="-200" y="-200" z="300" />
+          </feSpecularLighting>
+
+          <feComposite
+            in="specLight"
+            operator="arithmetic"
+            k1="0"
+            k2="1"
+            k3="1"
+            k4="0"
+            result="litImage"
+          />
+
+          <feDisplacementMap
+            in="SourceGraphic"
+            in2="softMap"
+            scale="150"
+            xChannelSelector="R"
+            yChannelSelector="G"
+          />
+        </filter>
+      </svg>
 
       <section id='home' className='titulos_secciones'>
         Seccion Inicio
@@ -32,8 +96,11 @@ function App() {
       <section id='contact' className='titulos_secciones'>
         Seccion Contacto
       </section>
+
+
     </div>
 
+    
 
   )
 }
