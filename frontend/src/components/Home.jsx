e1e6333480decb705b36ee29530b5b418f14b248
Merch-A <95261605+ElMercH1@users.noreply.github.com>
2025-07-24 14:17:53 -0500

    UPDATE - NUEVA VISTA PRINCIPAL

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -3,8 +3,32 @@ import '../CSS/Home/index.css'
 function Home() {
   return (
     <section id='home' className='titulos_secciones'>
-      <div id='Nombre'>
-        <h1>Leonardo Gallegos</h1>
+      <div id='contenedor_pantalla_home'>
+        
+        <div id='contenedor_texto_nombre'>
+          <texto_main>Leonardo Gallegos</texto_main>
+        </div>
+
+        <div id='contenedor_linea_divisora'>
+          <div className='linea_divisora'></div>
+        </div>
+
+        <div id='contenedor_texto_subtitulo'>
+          
+          <div id='texto_subtitulo'>
+            
+            <p className='subtitulo-desktop'>Ingeniero en Datos | Desarrollador Web | Desarrollador de Software</p>
+            
+            <div className='subtitulo-mobile'>
+              <p>Ingeniero en Datos</p>
+              <p>Desarrollador Web</p>
+              <p>Desarrollador de Software</p>
+              
+            </div>          
+          </div>
+
+        </div>
+
       </div>
     </section>
   );
