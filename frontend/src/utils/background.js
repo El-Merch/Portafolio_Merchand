eb23d83d563bc4f23e48d2d526539f34e141e3cc
LeonardoG <fin.merchand@gmail.com>
2025-08-12 19:42:19 -0500

    SAVE - BALL SPEED

--- frontend/src/utils/background.js
+++ frontend/src/utils/background.js
@@ -55,7 +55,7 @@ balls.forEach((el, i, ra) => {
       { transform: `translate(${to.x}rem, ${to.y}rem)` }
     ],
     {
-      duration: (Math.random() + 1) * 2000, // random duration
+      duration: (Math.random() + 1) * 5000, // random duration
       direction: "alternate",
       fill: "both",
       iterations: Infinity,
